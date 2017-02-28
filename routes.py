a209378b599f439ec2f7d9c9ad373cb1308dc91d
kaveer harie <kaveerharie@gmail.com>
2017-02-28 22:19:35 +0200

    update as to fix route

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ from forms import SignupForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mypythonapp:nVDCaEVM3/w@127.0.0.1/login'
 db.init_app(app)
 
 app.secret_key = "development-key"
@@ -34,4 +34,4 @@ def signup():
     return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+  app.run(debug=True,host='0.0.0.0')
